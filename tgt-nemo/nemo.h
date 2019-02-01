8a209a562808e175f5b0d8a7bf1db303c86508d9
Timothy Trippel <timothy.trippel@ll.mit.edu>
2019-02-01 12:26:08 -0500

    removed header comment

--- tgt-nemo/nemo.h
+++ tgt-nemo/nemo.h
@@ -1,14 +1,5 @@
 #ifndef __NEMO_HEADER__
 #define __NEMO_HEADER__
-/*
- * TTB Backend
- *
- * This is a backend for Icarus verilog which generates a dot file with a
- * dependancy graph showing which signals are dependant on which signals
- *
- * Currently the dot file format is not supported. Instead, I output our own
- * format.
- */
 
 #include <set>
 #include <vector>
